bd9e730969e023dbfb211ad45863fd678edda0cf
code4human <tena.kim10@gmail.com>
2021-01-31 07:30:15 +0900

    Define a struct for format tag and create ft_printf_print_text() for writing text until '%'

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -10,9 +10,42 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-struct			s_data
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# define TAG_FLAGS "-+ #0"
+# define TAG_LENGTH_HH 1
+# define TAG_LENGTH_H 2
+# define TAG_LENGTH_L 3
+# define TAG_LENGTH_LL 4
+# define TAG_SPECIFIERS "cspdiuxXnfge%"
+# define DIGIT_STR "0123456789"
+# define HEX_STR "0123456789abcdef"
+# define HEX_STR_UPPER "0123456789ABCDEF"
+
+# include "libft.h"
+# include <stdarg.h>
+# include <stdbool.h>
+
+typedef struct	s_data
 {
 	va_list		ap;
 	const char	*format;
 	int			len;
 }				t_data;
+
+typedef struct	s_format_tag
+{
+	bool	flag_sign;
+	bool	flag_left;
+	bool	flag_space;
+	bool	flag_sharp;
+	bool	flag_zero;
+	int		width;
+	int		precision;
+	char	length;
+	char	specifier;
+}				t_format_tag;
+
+
+#endif
--- srcs/ft_printf.c
+++ srcs/ft_printf.c
@@ -10,12 +10,38 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-int			ft_printf_print(t_data *data)
-{
+#include "ft_printf.h"
 
+static bool		ft_printf_print_text(t_data *data, size_t write_len)
+{
+	int len;
+
+	if (!data || write_len < 0)
+		return (false);
+	len = write(1, data.format, write_len);
+	if (len < 0)
+		return (false);
+	data.len += len;
+	return (true);
 }
 
-int			ft_printf(const char *format, ...)
+static int		ft_printf_print(t_data *data)
+{
+	t_format_tag	*tag;
+	char			*tag_start;
+	int				result;
+
+	while (!(tag_start = ft_strchr(data.format, '%')))
+	{
+		if (!ft_printf_print_text(data, tag_start - data.format))
+			return (-1);
+	}
+	if (!ft_printf_print_text(data, ft_strlen(data.format)))
+		return (-1);
+	return (data.len);
+}
+
+int		ft_printf(const char *format, ...)
 {
 	t_data	data;
 
