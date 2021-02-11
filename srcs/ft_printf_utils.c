eeda4d03f1062f96216ceb03ba23a7ecfe76de99
code4human <tena.kim10@gmail.com>
2021-02-11 18:30:20 +0900

    Implement ft_wchars_to_str to convert the wide character into a string and utils func to get wide string length

--- srcs/ft_printf_utils.c
+++ srcs/ft_printf_utils.c
@@ -48,3 +48,31 @@ char		*ft_strchrset(const char *s, const char *set)
 	}
 	return (NULL);
 }
+
+size_t		ft_wchar_utf8_len(wint_t c)
+{
+	if (c < 128)
+		return (1);
+	if (c < 2048)
+		return (2);
+	if (c < 65536)
+		return (3);
+	if (c < 2097152)
+		return (4);
+	return (0);
+}
+
+size_t		ft_wstr_utf8_len(const wchar_t *s)
+{
+	size_t	len;
+	size_t	i;
+
+	len = 0;
+	i = 0;
+	while (s[i] != L'\0')
+	{
+		len += ft_wchar_utf8_len(s[i]);
+		i++;
+	}
+	return (len);
+}
