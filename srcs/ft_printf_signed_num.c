840b7dfd185a991cdec0d0dc822369f150ae813c
code4human <tena.kim10@gmail.com>
2021-02-19 04:52:27 +0900

    Apply the sign of signed number to string and move the sign before ft_printf_putstr_fd()

--- srcs/ft_printf_signed_num.c
+++ srcs/ft_printf_signed_num.c
@@ -1,5 +1,43 @@
 #include "ft_printf.h"
 
+static bool	ft_add_sign(char **str, size_t *len,
+	t_format_tag *tag, bool is_negative)
+{
+	char	*temp;
+
+	if ((str == NULL) || (*str == NULL) || (tag == NULL))
+		return (-1);
+	if (!(tag->flag_sign) && !(tag->flag_space) && !(is_negative))
+		return (true);
+	if (!(temp = (char *)malloc(*len + 2)))
+		return (false);
+	*len += 1;
+	temp[0] = tag->flag_sign ? '+' : '-';
+	temp[0] = is_negative ? '-' : temp[0];
+	ft_strlcpy(temp + 1, *str, *len);
+	free(*str);
+	*str = temp;
+	return (true);
+}
+
+static int	ft_move_sign(char *str, t_format_tag *tag, bool is_negative)
+{
+	char	*sign_addr;
+	char	c;
+
+	if ((str == NULL) || (*str == NULL))
+		return (-1);
+	if (!(tag->flag_zero) || 
+		!(tag->flag_sign) && !(tag->flag_space) && !(is_negative))
+		return (true);
+	if (!(sign_addr = ft_strchrset(str, tag->flag_sign ? "+- " : "+-")))
+		return (false);
+	c = *sign_addr;
+	*sign_addr = str[0];
+	str[0] = c;
+	return (true);
+}
+
 static int	ft_write_signed_num(char **str, t_format_tag *tag, t_data *data)
 {
 	size_t	len;
@@ -20,7 +58,9 @@ static int	ft_write_signed_num(char **str, t_format_tag *tag, t_data *data)
 	}
 	len = ft_strlen(*str);
 	if (!ft_apply_precision(str, &len, tag)
-		|| !ft_apply_alignment(str, &len, tag))
+		|| !ft_add_sign(str, &len, tag, is_negative)
+		|| !ft_apply_alignment(str, &len, tag)
+		|| (tag->precision < 0) && !(ft_move_sign(*str, tag, is_negative)))
 		return (-1);
 	data->printf_len += len;
 	return (ft_printf_putstr_fd(*str, 1));
