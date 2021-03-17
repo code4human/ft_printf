a29bf7137fa3741f1a4b4a460d04a33befe6b54e
Taeeun Kim <taeekim@c3r1s3.42seoul.kr>
2021-03-18 04:50:43 +0900

    Apply norminette

--- srcs/ft_printf_unsigned_number.c
+++ srcs/ft_printf_unsigned_number.c
@@ -79,8 +79,8 @@ int			ft_printf_unsigned_number(
 	if ((tag == NULL) || (data == NULL))
 		return (-1);
 	arg = va_arg(data->ap, unsigned long long);
-        if (tag->length == TAG_LENGTH_HH)
-                str = ft_ulltoa_base((unsigned char)arg, base);
+	if (tag->length == TAG_LENGTH_HH)
+		str = ft_ulltoa_base((unsigned char)arg, base);
 	else if (tag->length == TAG_LENGTH_H)
 		str = ft_ulltoa_base((unsigned short)arg, base);
 	else if (tag->length == TAG_LENGTH_L)
